9a6890d24c96452cbc240d408a2fc3b9e8c9a5f9
JJSlumen <601535418@qq.com>
2018-09-30 10:19:45 +0800

    atm-springboot

---
+++ atm-springboot/src/main/java/com/dayuan/atmspringboot/AtmSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.dayuan.atmspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtmSpringbootApplication.class, args);
+    }
+}
